3374e3058c854e4a9ea3da64238c069ee54a1968
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-16 17:54:51 +0500

    added smooth scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,48 @@ modalWindowClosingButton.addEventListener("click", () => {
   document.body.style.overflow = "visible";
   container.style.opacity = "1";
 });
+
+const headerLinkSeries = document.querySelector(".header__link.series");
+headerLinkSeries.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const tvShows = document.querySelector(".topic__title.tv-shows");
+  const topOffset = headerLinkSeries.offsetHeight;
+  const elementPosition = tvShows.getBoundingClientRect().top;
+  const offsetPosition = elementPosition - topOffset;
+
+  window.scrollBy({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+});
+
+const headerLinkMovies = document.querySelector(".header__link.movies");
+headerLinkMovies.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const movies = document.querySelector(".topic__title.movies");
+  const topOffset = headerLinkMovies.offsetHeight;
+  const elementPosition = movies.getBoundingClientRect().top;
+  const offsetPosition = elementPosition - topOffset;
+
+  window.scrollBy({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+});
+
+const headerLinkCartoons = document.querySelector(".header__link.cartoons");
+headerLinkCartoons.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const cartoons = document.querySelector(".topic__title.cartoons");
+  const topOffset = headerLinkCartoons.offsetHeight;
+  const elementPosition = cartoons.getBoundingClientRect().top;
+  const offsetPosition = elementPosition - topOffset;
+
+  window.scrollBy({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+});
