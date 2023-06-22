ab338825d27e23f659e23ec54ef520ead161eecf
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-23 01:05:52 +0500

    refactored all code, imported data from data.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,81 +1,116 @@
+import data from "./data.js";
+
 const modalWindow = document.querySelector(".modal-window");
 const modalWindowClosingButton = document.querySelector(
   ".modal-closing__button"
 );
-const headerSignInButton = document.querySelector(".header__sign-in-button");
+const modalWindowOpenButton = document.querySelector(".header__sign-in-button");
 const container = document.querySelector(".container");
 
-headerSignInButton.addEventListener("click", () => {
-  modalWindow.classList.add("modal-window-visible");
-  document.body.style.overflow = "hidden";
-  container.style.opacity = "0.5";
-});
-
-modalWindowClosingButton.addEventListener("click", () => {
-  modalWindow.classList.remove("modal-window-visible");
-  document.body.style.overflow = "visible";
-  container.style.opacity = "1";
-});
+const modalWindowDisplay = (btn) => {
+  btn.addEventListener("click", () => {
+    if (btn === modalWindowOpenButton) {
+      modalWindow.classList.add("modal-window-visible");
+      document.body.style.overflow = "hidden";
+      container.style.opacity = "0.5";
+    } else {
+      modalWindow.classList.remove("modal-window-visible");
+      document.body.style.overflow = "visible";
+      container.style.opacity = "1";
+    }
+  });
+};
+modalWindowDisplay(modalWindowOpenButton);
+modalWindowDisplay(modalWindowClosingButton);
 
 const headerLinkSeries = document.querySelector(".header__link.series");
-headerLinkSeries.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const tvShows = document.querySelector(".topic__title.tv-shows");
-  const topOffset = headerLinkSeries.offsetHeight;
-  const elementPosition = tvShows.getBoundingClientRect().top;
-  const offsetPosition = elementPosition - topOffset;
-
-  window.scrollBy({
-    top: offsetPosition,
-    behavior: "smooth",
-  });
-});
-
+const tvShowsTopicTitle = document.querySelector(".topic__title.tv-shows");
 const headerLinkMovies = document.querySelector(".header__link.movies");
-headerLinkMovies.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const movies = document.querySelector(".topic__title.movies");
-  const topOffset = headerLinkMovies.offsetHeight;
-  const elementPosition = movies.getBoundingClientRect().top;
-  const offsetPosition = elementPosition - topOffset;
-
-  window.scrollBy({
-    top: offsetPosition,
-    behavior: "smooth",
-  });
-});
-
+const moviesTopicTitle = document.querySelector(".topic__title.movies");
 const headerLinkCartoons = document.querySelector(".header__link.cartoons");
-headerLinkCartoons.addEventListener("click", (e) => {
-  e.preventDefault();
+const cartoonsTopicTitle = document.querySelector(".topic__title.cartoons");
 
-  const cartoons = document.querySelector(".topic__title.cartoons");
-  const topOffset = headerLinkCartoons.offsetHeight;
-  const elementPosition = cartoons.getBoundingClientRect().top;
-  const offsetPosition = elementPosition - topOffset;
+const smoothScrolling = (headerLink, topicTitle) => {
+  headerLink.addEventListener("click", (e) => {
+    e.preventDefault();
 
-  window.scrollBy({
-    top: offsetPosition,
-    behavior: "smooth",
+    const topOffset = headerLink.offsetHeight;
+    const elementPosition = topicTitle.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
   });
-});
+};
+
+smoothScrolling(headerLinkSeries, tvShowsTopicTitle);
+smoothScrolling(headerLinkMovies, moviesTopicTitle);
+smoothScrolling(headerLinkCartoons, cartoonsTopicTitle);
 
 const creationPic = document.querySelector(".creation-container");
 const creationLogoImg = document.querySelector(".creation-logo__img");
 
-setInterval(() => {
-  creationPic.style.backgroundImage = 'url("../img/euphoria-pic.png")';
-  creationLogoImg.src = "../img/euphoria-logo.png";
-}, 4000);
-setInterval(() => {
-  creationPic.style.backgroundImage = 'url("../img/hod-pic.png")';
-  // creationLogoImg.style.width = "500px";
-  // creationLogoImg.style.height = "100px";
-  creationLogoImg.src = "../img/hod-logo.png";
-}, 8000);
-setInterval(() => {
-  creationPic.style.backgroundImage = 'url("../img/Joel&Ellie.png")';
-  creationLogoImg.src = "../img/tlou-title.png";
-}, 12000);
+const pictureAppearing = (pic, logo, time) => {
+  setInterval(() => {
+    creationPic.style.backgroundImage = `url(${pic})`;
+    creationLogoImg.src = `${logo}`;
+    if (pic.includes("hod")) {
+      creationLogoImg.classList.add("creation-logo__img_smaller-picture");
+    } else {
+      creationLogoImg.classList.remove("creation-logo__img_smaller-picture");
+    }
+  }, time);
+};
+
+pictureAppearing("../img/euphoria-pic.png", "../img/euphoria-logo.png", 4000);
+pictureAppearing("../img/hod-pic.png", "../img/hod-logo.png", 8000);
+pictureAppearing("../img/Joel&Ellie.png", "../img/tlou-title.png", 12000);
+
+const cardsMovies = document.querySelector(".cards.movies");
+const cardsShows = document.querySelector(".cards.tv-shows");
+const cardsCartoons = document.querySelector(".cards.cartoons");
+
+const filteredArts = (arr, type) => arr.filter((el) => el.type === type);
+
+const displayTemplate = (el, container) => {
+  container.innerHTML += `
+  <div class="card" style="background-image: url(${el.image})">
+  <div class="card__container_opacity">
+    <div class="card__container_top-space">
+      <div class="rating-container">
+        <img class="card__star-img_top-space" src="../img/star.png" />
+        <p class="card__rate_top-space">${el.rating}</p>
+      </div>
+      <p class="card__year_top-space">${el.year}</p>
+    </div>
+    <div class="card__container_middle-space">
+      <p class="card__title-text_middle-space">${el.title}</p>
+      <p class="card__main-text_middle-space">
+        ${el.description}
+      </p>
+    </div>
+    <div class="card__container_down-space">
+      <div class="watch-button__container">
+        <img
+          class="card__play-button_down-space"
+          src="../img/play-button.png"
+        />
+        <div class="card__watch-text_down-space">watch</div>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+};
+
+const displayCards = (arr, type, container) => {
+  filteredArts(arr, type).map((el) => {
+    displayTemplate(el, container);
+  });
+};
+
+displayCards(data, "movie", cardsMovies);
+displayCards(data, "cartoon", cardsCartoons);
+displayCards(data, "show", cardsShows);
